befdc8161f8152d9227137ce966eb0e41c18f544
LekhaNov_GIT <9219770700@mail.ru>
2023-07-04 22:53:36 +0300

    снова

---
+++ 01.py
@@ -0,0 +1,13 @@
+num = 21
+
+print('Козерог-' + str(num))
+
+donut = int(input('понч?: '))
+coffee = int(input('кофе?: '))
+
+print('я потратил', donut + coffee)
+
+donut = input('понч?: ')
+coffee = input('кофе?: ')
+print('я потратил', int(donut) + int(coffee))
+# rrrrhyh
